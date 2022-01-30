af4dadad4432cd91a1671c57352584715a368a76
Sasipa <sasipa.philai@gmail.com>
2022-01-30 11:23:21 +0700

    commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import movieReducer from '../features/Movie/MovieSlice'
+import userSlice from '../features/user/userSlice';
+import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    movie: movieReducer,
+    user: userSlice
   },
 });
